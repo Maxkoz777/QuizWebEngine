0558e1b00f44c09f1e3273b47181dfa90a310ae5
Grigorii Kostarev <grik2001@gmail.com>
2021-08-26 11:49:34 +0300

    fix: refactor

---
+++ backend/src/test/java/com/example/quizwebengine/QuizWebEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizwebengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizWebEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

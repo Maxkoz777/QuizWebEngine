1afc5a8d1460327ccdced8d04b293722abda8036
m.kozhinov <m.kozhinov@tinkoff.ru>
2021-08-26 11:23:53 +0300

    Initial commit

---
+++ src/main/java/com/example/quizwebengine/QuizWebEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizwebengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizWebEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizWebEngineApplication.class, args);
+    }
+
+}

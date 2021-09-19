b036b827b551b51a2bf5815d1c99798601fc3780
Maxim <maxkozhinov777@gmail.com>
2021-09-19 22:20:29 +0300

    UserRepository added

---
+++ backend/src/main/java/com/example/quizwebengine/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.quizwebengine.repository;
+
+import com.example.quizwebengine.model.userInfo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserById(Long id);
+
+}

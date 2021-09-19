dbabe778aea9c28e003a9ceb2f51b1e9ae98e4df
Maxim <maxkozhinov777@gmail.com>
2021-09-19 22:14:55 +0300

    JWTAuthenticationEntryPoint added

---
+++ backend/src/main/java/com/example/quizwebengine/security/JWTAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package com.example.quizwebengine.security;
+
+import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+
+        InvalidLoginResponse invalidLoginResponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(invalidLoginResponse);
+        httpServletResponse.setContentType(SecurityConstants.CONTENT_TYPE);
+        httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+        httpServletResponse.getWriter().println(jsonLoginResponse);
+
+    }
+}

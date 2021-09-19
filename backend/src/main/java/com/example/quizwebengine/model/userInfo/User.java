ee6331a297c0eab58af08df932a70f1adddfdf9e
Maxim <maxkozhinov777@gmail.com>
2021-09-19 22:02:45 +0300

    User entity updated

--- backend/src/main/java/com/example/quizwebengine/model/userInfo/User.java
+++ backend/src/main/java/com/example/quizwebengine/model/userInfo/User.java
@@ -1,26 +1,96 @@
 package com.example.quizwebengine.model.userInfo;
 
-import com.example.quizwebengine.model.quiz.Quiz;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.Date;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
+@NoArgsConstructor
 @Entity
-@Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, updatable = false)
     private String username;
 
-    private Role role;
+    @Column(nullable = false)
+    private String lastname;
 
-    @OneToMany
-    private List<Quiz> quizzes;
+    @Column(unique = true)
+    private String email;
 
+    @Column(columnDefinition = "text")
+    private String bio;
+
+    @Column(length = 3000)
+    private String password;
+
+    @ElementCollection(targetClass = Role.class)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(columnDefinition = "user_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name")
+    private Set<Role> roles = new HashSet<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = LocalDateTime.now();
+    }
+
+    public User(Long id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    /**
+     * Security
+     */
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }

ea680f861d763d64008084b7bf490f8b32ad1fae
Maxim <maxkozhinov777@gmail.com>
2021-09-25 16:39:34 +0300

    Image entity added

---
+++ backend/src/main/java/com/example/quizwebengine/model/util/ImageModel.java
@@ -0,0 +1,29 @@
+package com.example.quizwebengine.model.util;
+
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class ImageModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] imageBytes;
+
+    @JsonIgnore
+    private Long userId;
+
+    @JsonIgnore
+    private Long quizId;
+
+}

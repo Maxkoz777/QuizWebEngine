21e618f993fec7a80d9488a3f8bc89262b901a01
Maxim <maxkozhinov777@gmail.com>
2021-10-10 15:30:37 +0300

    Refactoring

--- backend/src/main/java/com/example/quizwebengine/payload/request/AnswerRequest.java
+++ backend/src/main/java/com/example/quizwebengine/payload/request/AnswerRequest.java
@@ -1,8 +1,12 @@
 package com.example.quizwebengine.payload.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnswerRequest {
 
     private String answerText;

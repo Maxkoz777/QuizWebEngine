aebed856d22bc0a3a3a5cf854339c2d7eafb6b2b
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-10-23 21:30:47 +0300

    removed unused import

--- backend/src/main/java/com/example/quizwebengine/service/impl/CustomUserDetailsService.java
+++ backend/src/main/java/com/example/quizwebengine/service/impl/CustomUserDetailsService.java
@@ -3,7 +3,6 @@ package com.example.quizwebengine.service.impl;
 import com.example.quizwebengine.model.user_info.User;
 import com.example.quizwebengine.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;

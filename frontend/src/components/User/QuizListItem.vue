ed6e6e7a6b2603a85de292c94f10ab059b58126e
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-12-18 09:53:26 +0300

    issues resolved

--- frontend/src/components/User/QuizListItem.vue
+++ frontend/src/components/User/QuizListItem.vue
@@ -2,7 +2,7 @@
   <q-card-section class="quiz-field row items-center justify-between cursor-pointer" @click="visitQuiz(id)" v-ripple>
     <div>
       <div class="text-h6">{{ name }}</div>
-      <div class="text-subtitle3 text-grey-8">0 questions</div>
+      <div class="text-subtitle3 text-grey-8">{{ quizCount }} questions</div>
     </div>
     <div>
       <q-btn round flat icon="delete" class="icon-color q-ml-xs" size="lg" @click.stop="onDelete"/>
@@ -21,6 +21,10 @@ export default {
       required: true
     },
     id: {
+      type: Number,
+      required: true
+    },
+    quizCount: {
       type: Number,
       required: true
     }
